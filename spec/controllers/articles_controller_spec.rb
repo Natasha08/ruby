c01163c0dad400e66c31452799eaae29dac9b70c
natasha <natasha08@me.com>
2016-08-10 09:00:12 -0600

    deletes tutorial rspec tests. temporarily removes auth for testing.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -7,5 +7,10 @@ describe ArticlesController do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
   end
 end
