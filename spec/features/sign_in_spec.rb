e2efb905121e7a53343ac6c89e69579bbf1fed89
natasha <natasha08@me.com>
2016-08-10 07:32:11 -0600

    articlescontroller eds

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require 'articles_controller'
+
+describe ArticlesController do
+  it "should be successful" do
+    articles = ArticlesController.new
+    puts articles
+
+  end
+end
